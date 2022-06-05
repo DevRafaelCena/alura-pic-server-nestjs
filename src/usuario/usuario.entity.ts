b715c614527ff63a5d9a64b9f665c675203ab61a
Rafael Cena <rafael.cena@hotmail.com>
2022-06-05 13:56:01 -0300

    serialização

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude, Expose } from "class-transformer"
 import { IsEmail, IsNotEmpty, IsString } from "class-validator"
 import {IsNomeDeUsuario} from "./is-nome-de-usuario-unico.validator"
 
@@ -6,6 +7,7 @@ export class Usuario{
 
     public id: number
 
+    @Expose({ name: "userName" })
     @IsNotEmpty({ message: "O nome de usuário é obrigatório" })
     @IsString({ message: "O nome de usuário deve ser uma string" })
     @IsNomeDeUsuario({ message: "O nome de usuário deve ser único" })
@@ -14,11 +16,17 @@ export class Usuario{
     @IsEmail({}, { message: "Email inválido" })
     public email: string
 
+    @Expose({ name: "password" })
+    @Exclude({ toPlainOnly: true })
     @IsNotEmpty({ message: "A senha é obrigatória" })
     public senha: string
 
+
+    @Expose({ name: "fullName" })
     @IsNotEmpty({ message: "O nome completo é obrigatório" })
     public nomeCompleto: string
 
+
+    @Expose({ name: "joinDate" }) // alias para dataDeEntrada'
     public dataDeEntrada: Date
 }
