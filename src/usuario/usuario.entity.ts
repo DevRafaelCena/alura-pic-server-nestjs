b2813ab0ff4162213f5c5f0ac05f815eb5c2c29d
Rafael Cena <rafael.cena@hotmail.com>
2022-06-05 10:40:18 -0300

    implementando validação

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,10 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
 
 
 export class Usuario{
+
     public id: number
+
+    @IsNotEmpty({ message: "O nome de usuário é obrigatório" })
+    @IsString({ message: "O nome de usuário deve ser uma string" })
     public nomeDeUsuario: string
+
+    @IsEmail({}, { message: "Email inválido" })
     public email: string
+
+    @IsNotEmpty({ message: "A senha é obrigatória" })
     public senha: string
+
+    @IsNotEmpty({ message: "O nome completo é obrigatório" })
     public nomeCompleto: string
+
     public dataDeEntrada: Date
 }
